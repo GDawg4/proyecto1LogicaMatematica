1e7766b8852f2778c8f654e16296c15ce1b2600b
gera1013 <36525807+gera1013@users.noreply.github.com>
2020-08-14 02:00:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,16 @@
 
 # Proyecto 1 - Cierre Transitivo
 
+# Las funciones son aplicaciones de la teoría extraída de: Rosen, K. H. (2011). Discrete Mathematics and Its Applications (7.a ed.)
+
 import numpy as np
 
 # definimos la clase grafo
 class Grafo:
 
   # inicialización del objeto
-  def __init__(self, vertices):
-    self.V = vertices
+  def __init__(self, v):
+    self.size = v
 
   # función para imprimir la matriz 
   def printSolution(self, solutionMatrix, mode): 
@@ -19,8 +21,8 @@ class Grafo:
       print ("El cierre transitivo por matrices para el grafo es: ") if mode == 1 else 
       print ("El cierre transitivo por Warshall para el grafo es: ") 
     )
-    for i in range(self.V): 
-      for j in range(self.V): 
+    for i in range(self.size): 
+      for j in range(self.size): 
         print (solutionMatrix[i][j], end =' ')
       print(' ')
 
@@ -28,8 +30,8 @@ class Grafo:
   def logicOr(self, m1, m2):
     res = [[0 for x in i] for i in m1]
 
-    for x in range(self.V):
-      for y in range(self.V):
+    for x in range(self.size):
+      for y in range(self.size):
         res[x][y] = 0 if m1[x][y] == 0 and m2[x][y] == 0 else 1
     
     return res
@@ -37,11 +39,12 @@ class Grafo:
   # se calcula el cierre transitivo del grafo usando matrices
   # se calculan las matrices booleanas para R2, R3, ..., Rn
   # luego se hace la operación V entre cada una de ellas para obtener la matriz de cierre transitivo
-  def cierreTransitivo(self, grafo):
+  # ref. 
+  def matricesBooleanas(self, grafo):
     mx_1 = grafo
     mx = mx_1
 
-    for x in range(self.V):
+    for x in range(self.size):
       mx_1 = np.matmul(mx_1, grafo)
       mx_1 = [[1 if x >= 1 else 0 for x in i] for i in mx_1]
 
@@ -50,19 +53,19 @@ class Grafo:
     self.printSolution(mx, 1)
   
   # se calcula el cierre transitivo del grafo con el algoritmo de Warshall
-  def warshall(self, graph):
-    reach =[i[:] for i in graph]
+  def warshall(self, grafo):
+    reach =[i[:] for i in grafo]
 
-    for k in range(self.V):
-      for i in range(self.V):
-          for j in range(self.V):
+    for k in range(self.size):
+      for i in range(self.size):
+          for j in range(self.size):
               reach[i][j] = reach[i][j] or (reach[i][k] and reach[k][j])
     
     self.printSolution(reach, 2)
 
 # grafo representado en la matriz booleana R
 R = [
-	[1, 1, 0, 1],
+  	[1, 1, 0, 1],
 	[0, 1, 1, 0],
 	[0, 0, 1, 1],
 	[0, 0, 0, 1]
@@ -72,5 +75,6 @@ R = [
 grafo = Grafo(len(R))
 
 # se llama a los dos métodos para calcular el cierre transitivo
-grafo.cierreTransitivo(R)
+grafo.matricesBooleanas(R)
 grafo.warshall(R)
+
